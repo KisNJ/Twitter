723f362f8070a06999575c8e6f1fdfb8d522842f
kisnj <knandor407@gmail.com>
2023-04-08 17:00:56 +0200

    Profile page with ssg helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,16 @@ import { api } from "~/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
 import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Twitter App</title>
+        <meta name="description" content="chirping" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster />
       <Component {...pageProps} />
     </ClerkProvider>
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,4 +1,3 @@
-import { type User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
@@ -11,6 +10,7 @@ import {
 
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
@@ -19,13 +19,7 @@ const ratelimit = new Ratelimit({
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
+
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
